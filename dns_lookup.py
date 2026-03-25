41434b2dd4dcd43fa73dc0710bff2e0964eb97b9
Vijay M <mpvj8734@gmail.com>
2026-03-25 23:22:56 +0530

    Add DNS file report

--- dns_lookup.py
+++ dns_lookup.py
@@ -1,13 +1,18 @@
 import dns.resolver
+from datetime import datetime
 
 domain = input("Enter a domain name: ")
 
 record_types = ["A", "AAAA", "CNAME", "MX", "NS", "TXT"]
 
+timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+filename = f"dns_report_{timestamp}.txt"
+
 for record in record_types:
     try:
         answers = dns.resolver.resolve(domain, record)
         print(f"\n🔹 {record} records for {domain}:")
+        file.write(f"\n🔹 {record} records for {domain}:\n")
 
         for rdata in answers:
             if record == "MX":
